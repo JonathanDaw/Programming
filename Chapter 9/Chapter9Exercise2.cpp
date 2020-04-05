d51bece5a067029ff3f57315dc9429e4c7497c48
JonathanDaw <56832218+JonathanDaw@users.noreply.github.com>
2020-04-05 14:31:34 -0400

    Update Chapter9Exercise2.cpp

--- Chapter 9/Chapter9Exercise2.cpp
+++ Chapter 9/Chapter9Exercise2.cpp
@@ -1,11 +1,11 @@
 /*
-Write a program that reads students’ names followed by their test scores. The program should output each student’s name followed by the test scores and the relevant grade. It should also find and print the highest test score and the name of the students having the highest test score.
+Write a program that reads studentsâ€™ names followed by their test scores. The program should output each studentâ€™s name followed by the test scores and the relevant grade. It should also find and print the highest test score and the name of the students having the highest test score.
 
 Student data should be stored in a struct variable of type studentType, which has four components: studentFName and studentLName of type string, testScore of type int (testScore is between 0 and 100), and grade of type char. Suppose that the class has 20 students. Use an array of 20 components of type studentType.
 
 Your program must contain at least the following functions:
 
-A function to read the students’ data into the array.
+A function to read the studentsâ€™ data into the array.
 
 A function to assign the relevant grade to each student.
 
@@ -13,7 +13,7 @@ A function to find the highest test score.
 
 A function to print the names of the students having the highest test score.
 
-Your program must output each student’s name in this form: last name followed by a comma, followed by a space, followed by the first name; the name must be left justified. Moreover, other than declaring the variables and opening the input and output files, the function main should only be a collection of function calls.
+Your program must output each studentâ€™s name in this form: last name followed by a comma, followed by a space, followed by the first name; the name must be left justified. Moreover, other than declaring the variables and opening the input and output files, the function main should only be a collection of function calls.
 */
 #include <iostream>
 #include <string>
@@ -74,8 +74,8 @@ void assignGrade(studentType student[]) {
 }
 int highestGrade(int& topScore, studentType student[]) {
     for (int i = 0; i < listSize; i++) {
-        if (student[i].grade > topScore) {
-            topScore = student[i].grade;
+        if (student[i].testScore > topScore) {
+            topScore = student[i].testScore;
         }
     }
     return topScore;
@@ -88,7 +88,7 @@ void printStudents(int& topScore, studentType student[]) {
 
     cout << "The students with the highest test scores are: ";
     for (int i = 0; i < listSize; i++) {
-        if (student[i].grade == topScore) {
+        if (student[i].testScore == topScore) {
             cout << student[i].studentLName << ", " << student[i].studentFName << endl;
         }
     }
